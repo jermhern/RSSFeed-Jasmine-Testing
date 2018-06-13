065dedaa5a47d21e64f134b4e0dfa778bc0f3350
Jeremy Hernandez <Jermhern@gmail.com>
2018-06-13 13:23:46 -0700

    fixed tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -29,46 +29,39 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
          it('have a defined URL', function() {
             let allFeedsURLs = [];
-            for (let feed of allFeeds) {allFeedsURLs.push(feed.url);}
-            expect(allFeedsURLs.length).toBe(4);
+            for (let feed of allFeeds) {expect(feed.url.length).toBeGreaterThan(0);}
          });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
          it('have a defined name', function() {
-            let allFeedsNames = [];
-            for(let feed of allFeeds) {allFeedsNames.push(feed.name);}
-            expect(allFeedsNames.length).toBe(4);
-         })
+            for(let feed of allFeeds) {expect(feed.name.length).toBeGreaterThan(0);}
+         });
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* a new test suite named "The menu" */
     describe('The menu', function() {
 
-        /* TODO: Write a test that ensures the menu element is
+        /* a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
          it('element is hidden by default', function() {
-            let isMenuHidden = false;
-            if(document.querySelector('.menu-hidden').classList.value === 'menu-hidden') {
-                isMenuHidden = true;
-            }
-            expect(isMenuHidden).toBe(true);
+            expect($('body').hasClass('menu-hidden')).toBe(true);
          });
 
-         /* TODO: Write a test that ensures the menu changes
+         /* a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
@@ -88,31 +81,26 @@ $(function() {
           });
 
     });
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
-        /* TODO: Write a test that ensures when the loadFeed
+        /* a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
          */
          beforeEach(function(done) {
             loadFeed(0, done);
          });
 
-         it('should have at least one .entry element in .feed', function() {
-            
-            feedEntry = document.querySelector('.feed').children.length;
-
-            expect(feedEntry).not.toBe(0);
+         it('should have at least one .entry element in .feed', function(done) {
+            expect($('.feed .entry').length).toBeGreaterThan(0);
+            done();
          }); 
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /* a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function() {
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
          */
          beforeEach(function(done) {
             loadFeed(1, function(){
